46b95c308daa788541884dfb0a9c359b959ad9d8
David Winniford <dwinniford@gmail.com>
2021-04-27 10:52:02 -0500

    add accessors for user password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,20 @@
+require 'bcrypt'
 class User
   include Mongoid::Document
+  include BCrypt 
   field :name, type: String
-  field :password, type: String
+  field :password_digest, type: String
+
+  validates :name, presence: true, uniqueness: true, length: {minimum: 5, maximum: 20}
+  validates :password, presence: true, length: {minimum: 5, maximum: 20}
+
+  def password
+    @password ||= Password.new(password_digest)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_digest = @password
+  end
+
 end
