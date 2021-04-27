b3120991ac48eae3b617cba4135f593a06dcc162
David Winniford <dwinniford@gmail.com>
2021-04-27 13:53:42 -0500

    add sign in mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     # Auth
     field :sign_up, mutation: Mutations::Auth::SignUp
+    field :sign_in, mutation: Mutations::Auth::SignIn
   end
 end
