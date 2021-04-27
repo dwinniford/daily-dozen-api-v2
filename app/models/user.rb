fe793f8bae82134288d6281b3e240eda7865c48c
David Winniford <dwinniford@gmail.com>
2021-04-27 11:50:32 -0500

    test passes validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User
   field :name, type: String
   field :password_digest, type: String
 
-  validates :name, presence: true, uniqueness: true, length: {minimum: 5, maximum: 20}
-  validates :password, presence: true, length: {minimum: 5, maximum: 20}
+  validates :name, presence: true, uniqueness: true, length: {minimum: 5, maximum: 40}
+  validates :password, presence: true, length: {minimum: 5, maximum: 72}
 
   def password
     @password ||= Password.new(password_digest)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it "has a valid factory" do
     user = FactoryBot.build(:user)
+    # byebug
+    # pp user
     expect(user.valid?).to be_truthy
+    
   end
 end
