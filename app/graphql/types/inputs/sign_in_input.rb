e330cf9b370b49aac9cf2d35ff4b0ec711550362
David Winniford <dwinniford@gmail.com>
2021-04-28 11:50:02 -0500

    passes signin mutation test

--- app/graphql/types/inputs/sign_in_input.rb
+++ app/graphql/types/inputs/sign_in_input.rb
@@ -1,8 +1,9 @@
 module Types
     module Inputs
       class SignInInput < BaseInputObject
+        # graphql_name "UserSignInInput"
         argument :name, String, required: true, description: "User's Name"
         argument :password, String, required: true, description: "Password minimum 5 and maximum 72 characters long"
       end
     end
-  end
+end
