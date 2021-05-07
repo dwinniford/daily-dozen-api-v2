7fa8c3a4958445a50e3c13d4be44a5c603cb8985
David Winniford <dwinniford@gmail.com>
2021-05-07 16:09:27 -0500

    delete meal plan mutation

---
+++ app/graphql/mutations/meal_plans/delete_meal_plan.rb
@@ -0,0 +1,23 @@
+module Mutations 
+    module MealPlans
+        class DeleteMealPlan < BaseMutation
+            description "delete meal plan for user"
+
+            # inputs
+            argument :id, ID, required: true 
+
+            #outputs 
+            field :success, Boolean, null: false 
+
+            def resolve(id)
+                authenticate_user
+                meal_plan = context[:current_user].meal_plans.find(id)
+                if meal_plan && meal_plan.destroy
+                    {success: true}
+                else
+                    raise GraphQL:ExecutionError.new("there was an error deleting the meal plan")
+                end
+            end
+        end
+    end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,9 @@ module Types
     # Auth
     field :sign_up, mutation: Mutations::Auth::SignUp
     field :sign_in, mutation: Mutations::Auth::SignIn
+
+    # MealPlans
+    field :create_meal_plan, mutation: Mutations::MealPlans::CreateMealPlan
+    field :delete_meal_plan, mutation: Mutations::MealPlans::DeleteMealPlan
   end
 end
