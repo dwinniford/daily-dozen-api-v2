47554eb62c623a0c9b8a7167f69b69bf1b035361
David Winniford <dwinniford@gmail.com>
2021-05-10 09:19:28 -0500

    tests createMealPlan

---
+++ spec/graphql/mutations/meal_plans/create_meal_plan_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+module Mutations 
+    module MealPlans 
+        RSpec.describe CreateMealPlan, type: :request do 
+            describe '.resolve' do 
+                it 'creates a users meal plan' do 
+                    user = FactoryBot.create(:user)
+                    # method from rails helper
+                    headers = sign_in_test_headers user 
+
+                    query = <<~GQL 
+                    mutation {
+                        createMealPlan(input: {title: "test meal plan"}) {
+                            mealPlan {
+                                id
+                            }
+                        }
+                    }
+                    GQL
+                    post '/graphql', params: {query: query}, headers: headers
+                    expect(response).to have_http_status(200)
+                    json = JSON.parse(response.body)
+                    # byebug
+                    expect(json['data']['createMealPlan']['mealPlan']['id']).not_to be_nil 
+                end 
+            end
+        end
+    end
+
+end
