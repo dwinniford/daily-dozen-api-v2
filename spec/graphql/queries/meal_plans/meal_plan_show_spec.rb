60d9218d122cfa76c99ae3cbb4c2d4fed22d12ae
David Winniford <dwinniford@gmail.com>
2021-05-20 07:07:07 -0500

    passes meal plan show test

---
+++ spec/graphql/queries/meal_plans/meal_plan_show_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+module Queries 
+    module MealPlans 
+        RSpec.describe MealPlanShow, type: :request do 
+            describe ".resolve" do 
+                it "finds a meal plan by id" do 
+                    user = FactoryBot.create(:user)
+                    meal_plan = FactoryBot.create(:meal_plan, user_id: user.id, title: "user meal plan 2")
+                    headers = sign_in_test_headers user
+                    query = <<~GQL
+                    query {
+                        mealPlanShow(id: "#{meal_plan.id}") {
+                            id
+                            title
+                        }
+                    }
+                    GQL
+                    post '/graphql', params: {query: query}, headers: headers
+                    expect(response).to have_http_status(200)
+                    json = JSON.parse(response.body)
+                    byebug
+                    expect(json["data"]["mealPlanShow"]["id"]).to eql meal_plan._id.to_s 
+
+                end
+            end   
+        end
+    end
+end
