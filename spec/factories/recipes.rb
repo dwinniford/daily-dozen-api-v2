f7c0d547f12ca7beec8eb75b5d828f097c6e0d3a
David Winniford <dwinniford@gmail.com>
2021-05-26 10:08:37 -0500

    recipe passes model test

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -2,8 +2,10 @@ FactoryBot.define do
   factory :recipe do
     url { "MyString" }
     label { "MyString" }
-    ingredient_lines { "" }
+    ingredient_lines { ["1 carrot", "2 broccoli", "1 can beans"] }
     source { "MyString" }
     image { "MyString" }
+
+    association :meal_plan
   end
 end
