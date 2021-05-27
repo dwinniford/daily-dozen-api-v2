098f06facac4b0a7f9dde7b961466ccddf8caaad
David Winniford <dwinniford@gmail.com>
2021-05-27 16:00:13 -0500

    add recipe input type

---
+++ app/graphql/types/inputs/recipe_input.rb
@@ -0,0 +1,13 @@
+module Types 
+    module Inputs 
+        class RecipeInput < BaseInputObject 
+            argument :label, String, required: true, description: "Recipe label"
+            argument :url, String, required: true, description: "Recipe url"
+            argument :source, String, required: false, description: "Recipe source name"
+            argument :image, String, required: false, description: "Recipe image"
+            argument :ingredient_lines, [String], required: false, description: "List of recipe ingredients"
+            argument :meal_plan_id, ID, required: true, description: "ID of the parent meal plan"
+            
+        end
+    end
+end
