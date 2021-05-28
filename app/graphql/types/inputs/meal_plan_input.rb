7ecd4ef671ac213d4559a2421f2133b3a8b5d37a
David Winniford <dwinniford@gmail.com>
2021-05-28 10:11:53 -0500

    meal plan input uses recipe input type

--- app/graphql/types/inputs/meal_plan_input.rb
+++ app/graphql/types/inputs/meal_plan_input.rb
@@ -2,6 +2,8 @@ module Types
     module Inputs 
         class MealPlanInput < BaseInputObject
             argument :title, String, required: true, description: "Meal Plan title"
+            # don't use the RecipeInputType?
+            argument :recipes, [Types::Inputs::RecipeInput], required: false, description: "List of recipes in the meal plan"
         end
     end
 end
