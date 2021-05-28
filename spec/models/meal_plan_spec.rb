dfb411c3bb9936ba46f93d21d3da2920b389bc48
David Winniford <dwinniford@gmail.com>
2021-05-28 13:39:54 -0500

    remove test bot create meal plan with recipe

--- spec/models/meal_plan_spec.rb
+++ spec/models/meal_plan_spec.rb
@@ -4,10 +4,5 @@ RSpec.describe MealPlan, type: :model do
   it "has a valid factory" do 
     meal_plan = FactoryBot.build(:meal_plan)
     expect(meal_plan.valid?).to be_truthy
-  end 
-  it "has valid factory for building a meal plan with recipes" do 
-    meal_plan_with_recipes = FactoryBot.build(:meal_plan_with_recipes)
-    expect(meal_plan_with_recipes.valid?).to be_truthy
-  end 
-  
+  end   
 end
