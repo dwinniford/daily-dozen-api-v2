a5e2501ea7083e363df734840af6a42fc820c31e
David Winniford <dwinniford@gmail.com>
2021-05-28 10:11:23 -0500

    fix typo

--- app/graphql/types/recipe_type.rb
+++ app/graphql/types/recipe_type.rb
@@ -3,7 +3,7 @@ module Types
     field :id, ID, null: false, description: "mongo db id for recipe"
     field :label, String, null: false, description: "Label of the recipe"
     field :url, String, null: false, description: "url of the recipe"
-    field :source, String, null: false, desciption: "Name of the source for the recipe"
+    field :source, String, null: false, description: "Name of the source for the recipe"
     field :ingredient_lines, [String], null: false, description: "List of ingredients for the recipe"
     field :image, String, null: false, description: "Image for the recipe"
     field :meal_plan, Types::MealPlanType, null: true, description: "Parent meal plan of the recipe"
